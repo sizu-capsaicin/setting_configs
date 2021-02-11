973e06f91c0571a486f6787af13d521b4d743389
sizu <sizu@inl.ics.keio.ac.jp>
2021-02-11 19:11:55 +0900

    make setup.sh

---
+++ zsh/setup.sh
@@ -0,0 +1,9 @@
+# install git-prompt & git-completion
+cd ~
+mkdir ~/.zsh
+cd ~/.zsh
+
+curl -o git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+cd ~
